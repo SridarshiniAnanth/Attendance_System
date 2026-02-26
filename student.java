cadd0bc612a646058d308b3c2943e3fc256f27b3
SridarshiniAnanth <darshiananth005@gmail.com>
2026-02-26 14:29:26 +0530

    Add Student class with name and department fields

---
+++ student.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Student {
+
+    private String name;
+    private String department;
+
+    public Student(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}
